a040b75155b485ea01cd991dde12174350cb4664
Alessandro Curci <hantarex@gmail.com>
2016-12-30 16:52:12 +0100

    added amministrazione select2 field added custom empty selection for select2 fields implemented custom matcher and highlight for all select2 fields

--- src/app/registri/edit/registri-edit.component.ts
+++ src/app/registri/edit/registri-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, OnDestroy, ViewEncapsulation, Injector } from '@angular/core';
-import { Select2OptionData } from 'ng2-select2';
-import { __platform_browser_private__ } from '@angular/platform-browser'; // needed for select2 styles override hack
+import {Component, OnInit, OnDestroy, ViewEncapsulation, Injector} from '@angular/core';
+import {Select2OptionData} from 'ng2-select2';
+import {__platform_browser_private__} from '@angular/platform-browser'; // needed for select2 styles override hack
 
-import { Titolari, Fascicoli } from '../../_models/index';
-import { TitolariService, FascicoliService } from '../../_services/index';
+import {Titolari, Fascicoli, Amministrazione} from '../../_models/index';
+import {APICommonService} from '../../_services/index';
+import {AppConfig} from '../../app.config';
+
 
 @Component({
     templateUrl: 'registri-edit.component.html',
@@ -12,7 +14,7 @@ import { TitolariService, FascicoliService } from '../../_services/index';
 export class RegistriEditComponent implements OnInit, OnDestroy {
 
     model: any;
-
+    config: any;
     injector: Injector;
     domSharedStylesHost: any;
     selected: any;
@@ -20,13 +22,16 @@ export class RegistriEditComponent implements OnInit, OnDestroy {
     titolariSelect: Select2OptionData[] = [];
     fascicoli: Fascicoli[] = [];
     fascicoliSelect: Select2OptionData[] = [];
+    amministrazione: Amministrazione[] = [];
+    amministrazioneSelect: Select2OptionData[] = [];
     date: Date = new Date(2016, 5, 10);
+    query: any;
 
-    constructor(
-        injector: Injector,
-        private titolariService: TitolariService,
-        private fascicoliService: FascicoliService
-    ) {
+    public select2Options: Select2Options;
+
+    constructor(injector: Injector, private apiService: APICommonService, config: AppConfig) {
+
+        this.select2Options = config.select2Options;
 
         //
         // This is a hack on angular style loader to prevent ng2-select2 from adding its styles.
@@ -39,27 +44,42 @@ export class RegistriEditComponent implements OnInit, OnDestroy {
             if (!style || !style.trim().startsWith(".select2-container")) {
                 this.domSharedStylesHost.__onStylesAdded__(additions);
             }
-        }
+        };
     }
 
     ngOnInit() {
-        this.titolariService.getAll().subscribe(titolari => {
+
+        this.apiService.getAll('titolari').subscribe(titolari => {
             this.titolari = titolari;
             this.titolariSelect = titolari as Select2OptionData[];
             this.titolariSelect.forEach((entry) => {
-               entry.text = entry['codice'] + ' - ' + entry['denominazione'] + ' - ' + entry['descrizione'];
+                entry.text = entry['codice'] + ' - ' + entry['denominazione'] + ' - ' + entry['descrizione'];
+                entry['id'] = entry['id'];
             });
+            this.titolariSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+
 
         });
 
-        this.fascicoliService.getAll().subscribe(fascicoli => {
-            let count = 0;
+        this.apiService.getAll('fascicoli').subscribe(fascicoli => {
             this.fascicoli = fascicoli;
             this.fascicoliSelect = fascicoli as Select2OptionData[];
             this.fascicoliSelect.forEach((entry) => {
                 entry['text'] = entry['numero_fascicolo'] + ' - ' + entry['argomento'];
-                entry['id'] = String(count++);
+                entry['id'] = entry['numero_fascicolo'];
             });
+            this.fascicoliSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+
+        });
+
+        this.apiService.getAll('amministrazione').subscribe(amministrazione => {
+            this.amministrazione = amministrazione;
+            this.amministrazioneSelect = amministrazione as Select2OptionData[];
+            this.amministrazioneSelect.forEach((entry) => {
+                entry['text'] = entry['codice'] + ' - ' + entry['denominazione'];
+                entry['id'] = entry['id'];
+            });
+            this.amministrazioneSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
 
         });
     }
@@ -75,6 +95,10 @@ export class RegistriEditComponent implements OnInit, OnDestroy {
 
     getFascicoli(): Select2OptionData[] {
         return this.fascicoliSelect;
+    }
+
+    getAmministrazione(): Select2OptionData[] {
+        return this.amministrazioneSelect;
     }
 
     select2Changed(e: any): void {
