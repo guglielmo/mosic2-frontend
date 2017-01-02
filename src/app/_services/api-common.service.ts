a771a9081684e446db562983bf4ed415fd008d83
Alessandro Curci <hantarex@gmail.com>
2017-01-02 19:04:07 +0100

    enhanced APICommonService to load basic data only once and provide it app wide

--- src/app/_services/api-common.service.ts
+++ src/app/_services/api-common.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Select2OptionData } from 'ng2-select2';
+
+import {Titolari, Fascicoli, Amministrazione, Mittente} from '../_models/index';
 
 import {AppConfig} from '../app.config';
 
@@ -7,8 +10,26 @@ import {AppConfig} from '../app.config';
 export class APICommonService {
     config: any;
 
+    titolari: Titolari[] = [];
+    titolariSelect: Select2OptionData[] = [];
+    titolariEnum: any = {};
+
+    fascicoli: Fascicoli[];
+    fascicoliSelect: Select2OptionData[] = [];
+
+    amministrazione: Amministrazione[] = [];
+    amministrazioneSelect: Select2OptionData[] = [];
+
+    mittente: Mittente[] = [];
+    mittenteSelect: Select2OptionData[] = [];
+
     constructor(private http: Http, config: AppConfig) {
         this.config = config.getConfig();
+        this.cacheData();
+    }
+
+    ngOnInit() {
+
     }
 
     getAll(apipath: string) {
@@ -29,6 +50,55 @@ export class APICommonService {
 
     delete(apipath: string, id: number) {
         return this.http.delete( this.config.baseAPIURL + '/api/'+apipath+'/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // PUBLIC helper methods
+
+    public cacheData() {
+        this.getAll('titolari').subscribe(titolari => {
+            this.titolari = titolari;
+            this.titolariSelect = Object.assign([], titolari) as Select2OptionData[];
+            this.titolariSelect.forEach((entry) => {
+                entry.text = entry['codice'] + ' - ' + entry['denominazione'] + ' - ' + entry['descrizione'];
+                entry['id'] = entry['id'];
+
+                this.titolariEnum[entry['id']] = entry['denominazione'];
+            });
+            this.titolariSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+        });
+
+        this.getAll('fascicoli').subscribe(fascicoli => {
+            fascicoli.forEach((entry) => {
+                entry['titolario'] = this.titolariEnum[entry['titolario']];
+            });
+            this.fascicoli = fascicoli;
+            this.fascicoliSelect = Object.assign([], fascicoli) as Select2OptionData[];
+            this.fascicoliSelect.forEach((entry) => {
+                entry['text'] = entry['numero_fascicolo'] + ' - ' + entry['argomento'];
+                entry['id'] = entry['numero_fascicolo'];
+            });
+            this.fascicoliSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+        });
+
+        this.getAll('amministrazione').subscribe(amministrazione => {
+            this.amministrazione = amministrazione;
+            this.amministrazioneSelect = Object.assign([], amministrazione) as Select2OptionData[];
+            this.amministrazioneSelect.forEach((entry) => {
+                entry['text'] = entry['codice'] + ' - ' + entry['denominazione'];
+                entry['id'] = entry['id'];
+            });
+            this.amministrazioneSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+        });
+
+        this.getAll('mittente').subscribe(mittente => {
+            this.mittente = mittente;
+            this.mittenteSelect = Object.assign([], mittente) as Select2OptionData[];
+            this.mittenteSelect.forEach((entry) => {
+                entry['text'] = entry['codice'] + ' - ' + entry['denominazione'];
+                entry['id'] = entry['id'];
+            });
+            this.mittenteSelect.unshift({id: '-1', text: 'Inizia a scrivere per selezionare...'});
+        });
     }
 
     // private helper methods
