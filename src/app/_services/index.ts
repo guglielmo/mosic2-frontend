8b333a812d3d509d83e7c69b182d6810854a1465
Alessandro Curci <hantarex@gmail.com>
2017-01-02 19:18:30 +0100

    drop titolari, fascicoli services

--- src/app/_services/index.ts
+++ src/app/_services/index.ts
@@ -1,7 +1,5 @@
 ﻿export * from './alert.service';
 export * from './authentication.service';
 export * from './api-common.service';
-export * from './titolari.service';
-export * from './fascicoli.service';
 export * from './registri.service';
 export * from './user.service';
