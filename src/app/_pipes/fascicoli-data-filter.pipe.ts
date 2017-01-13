521993eee516b52b8ad64cecc58caf8e96bf047c
Alessandro Curci <hantarex@gmail.com>
2017-01-13 15:55:04 +0100

    added data filter pipes for fascicoli and registri

---
+++ src/app/_pipes/fascicoli-data-filter.pipe.ts
@@ -0,0 +1,40 @@
+import * as _ from "lodash";
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+    name: "fascicoliDataFilter"
+})
+export class FascicoliDataFilterPipe implements PipeTransform {
+
+    transform(array: any[], query: string, titolari: number, amministrazione: number, numero_fascicolo: string): any {
+
+        console.log(query, titolari, amministrazione);
+
+        let keys = query.toUpperCase().split(' ');
+        let keysLen = keys.length;
+        let i;
+
+        // pre-compute some conditions to execute checks outside the loop
+        let qL = query.length > 2;
+        let tL = titolari != -1;
+        let aL = amministrazione != -1;
+        let fL = numero_fascicolo.length > 0;
+
+        return _.filter(array, row => {
+
+            if (tL && row.codice_titolario != titolari) return false;
+            if (aL && row.amministrazione != amministrazione) return false;
+            if (fL && row.numero_fascicolo != numero_fascicolo) return false;
+            if (qL) {
+                for (i = 0; i < keysLen; i++) {
+                    if ((row.argomento.toUpperCase()).indexOf((keys[i])) == -1) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        });
+
+    }
+}
---
+++ src/app/_pipes/registri-data-filter.pipe.ts
@@ -0,0 +1,49 @@
+import * as _ from "lodash";
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+    name: "registriDataFilter"
+})
+export class RegistriDataFilterPipe implements PipeTransform {
+
+    transform(array: any[],
+              id: string,
+              oggetto: string,
+              titolario: number,
+              mittente: number,
+              protocollo_mittente: string,
+              protocollo_arrivo: string,
+              numero_fascicolo: number,
+              data_arrivo_da: number,
+              data_arrivo_a: number): any {
+
+
+
+        let keys = oggetto.toUpperCase().split(' ');
+        let keysLen = keys.length;
+        let i;
+
+        // pre-compute some conditions to execute checks outside the loop
+        let qL = oggetto.length > 2;
+        let tL = titolario != -1;
+        let mL = mittente != -1;
+        let fL = numero_fascicolo > 0;
+
+        return _.filter(array, row => {
+            if (id && row.id != id) return false;
+            if (tL && row.titolario != titolario) return false;
+            if (mL && row.mittente != mittente) return false;
+            if (fL && row.numero_fascicolo != numero_fascicolo) return false;
+            if (qL) {
+                for (i = 0; i < keysLen; i++) {
+                    if ((row.oggetto.toUpperCase()).indexOf((keys[i])) == -1) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        });
+
+    }
+}
