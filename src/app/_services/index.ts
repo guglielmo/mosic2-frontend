1bc000dcbcad07d333c696d866ffc847980fe3e0
Alessandro Curci <hantarex@gmail.com>
2017-01-20 10:50:24 +0100

    dropped registri and user service (already replaced by APICommonService)

--- src/app/_services/index.ts
+++ src/app/_services/index.ts
@@ -1,5 +1,3 @@
 ﻿export * from './alert.service';
 export * from './authentication.service';
 export * from './api-common.service';
-export * from './registri.service';
-export * from './user.service';
