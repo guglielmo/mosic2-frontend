6a4ae1780d42a6011e8177ebaaaa532bddb06ab8
Alessandro Curci <hantarex@gmail.com>
2017-01-20 10:36:17 +0100

    updated for new API data model

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -23,9 +23,9 @@ export class AuthenticationService {
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let user = response.json();
-                if (user && user.token) {
+                if (user.data && user.data.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    localStorage.setItem('currentUser', JSON.stringify(user.data));
                 }
             });
     }
