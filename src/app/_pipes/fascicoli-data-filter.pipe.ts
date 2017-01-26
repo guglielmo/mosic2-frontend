c14bb0346b7d322d5cb55f1a31ab7ab4802a4e39
Alessandro Curci <hantarex@gmail.com>
2017-01-26 15:53:48 +0100

    added filter counter + refactorings

--- src/app/_pipes/fascicoli-data-filter.pipe.ts
+++ src/app/_pipes/fascicoli-data-filter.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from "@angular/core";
 })
 export class FascicoliDataFilterPipe implements PipeTransform {
 
-    transform(array: any[], query: string, id_titolari: number, amministrazione: number, numero_fascicolo: string): any {
+    transform(array: any[], query: string, id_titolari: number, id_amministrazioni: number, numero_fascicolo: string, filteredCount: any): any {
 
         let keys = query.toUpperCase().split(' ');
         let keysLen = keys.length;
@@ -15,13 +15,13 @@ export class FascicoliDataFilterPipe implements PipeTransform {
         // pre-compute some conditions to execute checks outside the loop
         let qL = query.length > 2;
         let tL = id_titolari != -1;
-        let aL = amministrazione != -1;
+        let aL = id_amministrazioni != -1;
         let fL = numero_fascicolo.length > 0;
 
-        return _.filter(array, row => {
+        let results = _.filter(array, row => {
 
             if (tL && row.id_titolari != id_titolari) return false;
-            if (aL && row.amministrazione != amministrazione) return false;
+            if (aL && row.id_amministrazioni.split(',').indexOf(id_amministrazioni) == -1) return false;
             if (fL && row.numero_fascicolo != numero_fascicolo) return false;
             if (qL) {
                 for (i = 0; i < keysLen; i++) {
@@ -33,6 +33,8 @@ export class FascicoliDataFilterPipe implements PipeTransform {
 
             return true;
         });
+        filteredCount.count = results.length;
+        return results;
 
     }
 }
