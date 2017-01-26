3bfbbeb2e3faa9665787fa02bdeaa3105b32a691
Alessandro Curci <hantarex@gmail.com>
2017-01-26 15:54:29 +0100

    added a call to prime the API cache

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { APICommonService } from '../_services/index';
+
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.template.html'
 })
 export class Dashboard {
+
+    constructor(private apiService: APICommonService) {
+
+
+    }
+
+    ngOnInit() {
+        this.apiService.refreshCommonCache();
+    }
+
+
 }
