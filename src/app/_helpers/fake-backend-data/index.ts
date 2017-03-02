474b6d31edbd5c58af5574673c51a4df455de104
Alessandro Curci <hantarex@gmail.com>
2017-03-02 15:52:04 +0100

    added lastupdate to directory index

--- src/app/_helpers/fake-backend-data/index.ts
+++ src/app/_helpers/fake-backend-data/index.ts
@@ -2,4 +2,5 @@
 export * from './fascicoli.mockdata'
 export * from './mittenti.mockdata';
 export * from './registri.mockdata';
-export * from './titolari.mockdata'
+export * from './titolari.mockdata';
+export * from './lastupdates.mockdata';
