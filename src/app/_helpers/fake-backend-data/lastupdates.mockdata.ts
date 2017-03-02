2db0485cf1bafd510d856184ffb224c614a4de9d
Alessandro Curci <hantarex@gmail.com>
2017-03-02 15:48:00 +0100

    updated lastupdates mockdata

---
+++ src/app/_helpers/fake-backend-data/lastupdates.mockdata.ts
@@ -0,0 +1,5 @@
+export const LastUpdatesMockData: any = {
+    "data": {
+        "titolari": 1485431762000, "fascicoli": 1487086660000, "registri": 1487255442000, "amministrazioni": 1485275424000, "mittenti": 1485275424000
+    }
+};
