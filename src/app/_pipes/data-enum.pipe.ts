193bf03f60cb4850103896d697fd827a25473caf
Alessandro Curci <hantarex@gmail.com>
2017-04-02 12:06:27 +0200

    Added optional separator argument to dataEnum pipe

--- src/app/_pipes/data-enum.pipe.ts
+++ src/app/_pipes/data-enum.pipe.ts
@@ -5,14 +5,14 @@ import * as _ from 'lodash';
 @Pipe({name: 'dataEnum'})
 export class DataEnumPipe implements PipeTransform {
 
-    transform( id: any, dataseries: any, apipath: string, prop: string ): string {
+    transform( id: any, dataseries: any, apipath: string, prop: string, separator: string = ', ' ): string {
 
         if (-1 !== String(id).indexOf(',') ) {
             const ret = [];
             String(id).split(',').forEach( item => {
                 ret.push(_.get( dataseries, apipath + '["' + item + '"]' + prop, ''));
             });
-            return ret.join(', ');
+            return ret.join(separator);
         }
 
         return _.get( dataseries, apipath + '["' + id + '"]' + prop, '');
