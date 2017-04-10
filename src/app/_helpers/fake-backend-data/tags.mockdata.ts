f6509cbb62ba259f90e36319d7ce3e062156c12d
Alessandro Curci <hantarex@gmail.com>
2017-04-10 18:29:01 +0200

    added tags mockdata

---
+++ src/app/_helpers/fake-backend-data/tags.mockdata.ts
@@ -0,0 +1,41 @@
+export const TagsMockData: any = {
+    "response": 200,
+    "total_results": 8,
+    "limit": "9999",
+    "offset": 0,
+    "data": [
+        {
+            "id": 2,
+            "denominazione": "secondo tags"
+        },
+        {
+            "id": 3,
+            "denominazione": "adfsdfsafas"
+        },
+        {
+            "id": 4,
+            "denominazione": "adfsdfsafas"
+        },
+        {
+            "id": 5,
+            "denominazione": "adfsdfsafas"
+        },
+        {
+            "id": 6,
+            "denominazione": "adfsdfsafas"
+        },
+        {
+            "id": 7,
+            "denominazione": "adfsdfsafas"
+        },
+        {
+            "id": 8,
+            "denominazione": "yeeeee"
+        },
+        {
+            "id": 10,
+            "denominazione": "test"
+        }
+    ]
+};
+
