810e69bcdd4a01d45a287d533e8c2f632a6d3bd2
Alessandro Curci <hantarex@gmail.com>
2017-04-10 18:34:04 +0200

    updated lastupdates mockdata + reformats

--- src/app/_helpers/fake-backend-data/lastupdates.mockdata.ts
+++ src/app/_helpers/fake-backend-data/lastupdates.mockdata.ts
@@ -1,5 +1,16 @@
 export const LastUpdatesMockData: any = {
     "data": {
-        "titolari": 1485431762000, "fascicoli": 1487086660000, "registri": 1487255442000, "amministrazioni": 1485275424000, "mittenti": 1485275424000
+        "titolari": 1491562373000,
+        "fascicoli": 1490052660000,
+        "registri": 1491838188000,
+        "amministrazioni": 1490486988000,
+        "mittenti": 1491479960000,
+        "tags": 1489079257000,
+        "uffici": 1488907760000,
+        "ruoli_cipe": 1488990889000,
+        "groups": 1488967625000,
+        "argomenti": 1488967625000,
+        "precipe": 1491818859000,
+        "precipeodg": 1488967625000
     }
 };
