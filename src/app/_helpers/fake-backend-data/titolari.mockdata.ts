51bba9ae25dd99f15323fe4947da3645f3da98c4
Alessandro Curci <hantarex@gmail.com>
2017-04-10 17:56:31 +0200

    updated titolari mockdata

--- src/app/_helpers/fake-backend-data/titolari.mockdata.ts
+++ src/app/_helpers/fake-backend-data/titolari.mockdata.ts
@@ -1,26 +1,77 @@
 export const TitolariMockData: any = {
     "response": 200,
-    "total_results": 21,
-    "data": [
-        {"id": 0, "codice": 0, "denominazione": 'Documenti di seduta', "descrizione": 'Telex, Appunto generale, passi, etc.'},
-        {"id": 1, "codice": 1, "denominazione": 'Segreteria Cipe', "descrizione": 'Competenze CIPE, Regolamento interno CIPE, Regolamento interno Commissioni, Altri organismi in seno al CIPE, ISTAT'},
-        {"id": 2, "codice": 2, "denominazione": 'Programmazione Finanziaria', "descrizione": 'Cofinanziamento programmi UE, Aree depresse, Fondo art.19 D.L.vo 96/93'},
-        {"id": 3, "codice": 3, "denominazione": 'Ambiente', "descrizione": 'Difesa acqua, difesa aria, difesa suolo, piano biodiversità'},
-        {"id": 4, "codice": 4, "denominazione": 'Energia', "descrizione": 'Elettrica, Fonti alternative, Metanizzazione, Nucleare, Risparmio energetica'},
-        {"id": 5, "codice": 5, "denominazione": 'Montagna', "descrizione": 'Montagna'},
-        {"id": 6, "codice": 6, "denominazione": 'Agricoltura', "descrizione": 'Agro-forestale, Agro-industriale, Pesca e acquacoltura, Zootecnia'},
-        {"id": 7, "codice": 7, "denominazione": 'Industria Commercio ed Artigianato', "descrizione": 'Attività estrattiva, Commercio e artigianato, Manifatturiere, Cooperazione int.le, Commercio estero'},
-        {"id": 8, "codice": 8, "denominazione": 'Programmazione negoziata', "descrizione": 'Accordi di programma, Contratti d\'area, Contratti di programma, Intese di programma, Patti territoriali'},
-        {"id": 9, "codice": 9, "denominazione": 'Occupazione e lavoro', "descrizione": 'Formazione professionale, Occupazione, Previdenza e assistenza, Tutela salariale'},
-        {"id": 10, "codice": 10, "denominazione": 'Ricerca e istruzione', "descrizione": 'Istruzione, Ricerca'},
-        {"id": 11, "codice": 11, "denominazione": 'Infrastrutture e trasporti', "descrizione": 'Aeroporti, Edilizia pubblica, Ferrovie, Interporti, Opere idriche, Porti, Strade e autostrade, Trasporto rapido di massa'},
-        {"id": 12, "codice": 12, "denominazione": 'Sanita', "descrizione": 'Edilizia sanitaria, Fondo Sanitario Nazionale (parte corrente e capitale)'},
-        {"id": 13, "codice": 13, "denominazione": 'Servizi', "descrizione": 'Assicurazioni, Credito, Turismo'},
-        {"id": 14, "codice": 14, "denominazione": 'Tariffe', "descrizione": 'Acqua e depurazione, Autostradali, Ferroviarie, Telefoniche e postali'},
-        {"id": 15, "codice": 15, "denominazione": 'Farmaci', "descrizione": 'Prezzi, Contenzioso'},
-        {"id": 16, "codice": 16, "denominazione": 'Terremoto', "descrizione": 'Terremoto'},
-        {"id": 17, "codice": 17, "denominazione": 'Gestione contabile', "descrizione": 'Spese in conto capitale, Spese di esercizio, Convenzioni ex Agensud'},
-        {"id": 18, "codice": 18, "denominazione": 'Organizzazione', "descrizione": 'Pianificazione e controllo, Gestione del personale, Procedure e informatizzazione'},
-        {"id": 19, "codice": 19, "denominazione": 'Attività diverse', "descrizione": 'Telecomunicazioni, FIO, Varie'}
-    ]
+    "total_results": 22,
+    "limit": "9999",
+    "offset": 0,
+    "data": [{"id": 1, "codice": 0, "denominazione": "Documenti di seduta", "descrizione": "Telex, Appunto generale, passi, etc."}, {
+        "id": 3,
+        "codice": 1,
+        "denominazione": "Segreteria Cipe",
+        "descrizione": "Competenze CIPE, Regolamento interno CIPE, Regolamento interno Commissioni, Altri organismi in seno al CIPE, ISTAT"
+    }, {
+        "id": 2,
+        "codice": 2,
+        "denominazione": "Programmazione Finanziaria",
+        "descrizione": "Cofinanziamento programmi UE, Aree depresse, Fondo art.19 D.L.vo 96\/93"
+    }, {
+        "id": 4,
+        "codice": 3,
+        "denominazione": "Ambiente",
+        "descrizione": "Difesa acqua, difesa aria, difesa suolo, piano biodiversit\u00e0"
+    }, {
+        "id": 5,
+        "codice": 4,
+        "denominazione": "Energia",
+        "descrizione": "Elettrica, Fonti alternative, Metanizzazione, Nucleare, Risparmio energetica"
+    }, {"id": 6, "codice": 5, "denominazione": "Montagna", "descrizione": "Montagna"}, {
+        "id": 7,
+        "codice": 6,
+        "denominazione": "Agricoltura",
+        "descrizione": "Agro-forestale, Agro-industriale, Pesca e acquacoltura, Zootecnia"
+    }, {"id": 8, "codice": 7, "denominazione": "Industria Commercio ed Artigianato", "descrizione": "Attivit\u00e0"}, {
+        "id": 9,
+        "codice": 8,
+        "denominazione": "Programmazione negoziata",
+        "descrizione": "Accordi di programma, Contratti d'area, Contratti di programma, Intese di programma, Patti territoriali"
+    }, {
+        "id": 10,
+        "codice": 9,
+        "denominazione": "Occupazione e lavoro",
+        "descrizione": "Formazione professionale, Occupazione, Previdenza e assistenza, Tutela salariale"
+    }, {"id": 11, "codice": 10, "denominazione": "Ricerca e istruzione", "descrizione": "Istruzione, Ricerca"}, {
+        "id": 12,
+        "codice": 11,
+        "denominazione": "Infrastrutture e trasporti",
+        "descrizione": "Aeroporti, Edilizia pubblica, Ferrovie, Interporti, Opere idriche, Porti, Strade e autostrade, Trasporto rapido di massa"
+    }, {
+        "id": 13,
+        "codice": 12,
+        "denominazione": "Sanit\u00e0",
+        "descrizione": "Edilizia sanitaria, Fondo Sanitario Nazionale (parte corrente e capitale)"
+    }, {"id": 14, "codice": 13, "denominazione": "Servizi", "descrizione": "Assicurazioni, Credito, Turismo"}, {
+        "id": 15,
+        "codice": 14,
+        "denominazione": "Tariffe",
+        "descrizione": "Acqua e depurazione, Autostradali, Ferroviarie, Telefoniche e postali"
+    }, {"id": 16, "codice": 15, "denominazione": "Farmaci", "descrizione": "Prezzi, Contenzioso"}, {
+        "id": 17,
+        "codice": 16,
+        "denominazione": "Terremoto",
+        "descrizione": "Terremoto"
+    }, {
+        "id": 18,
+        "codice": 17,
+        "denominazione": "Gestione contabile",
+        "descrizione": "Spese in conto capitale, Spese di esercizio, Convenzioni ex Agensud"
+    }, {
+        "id": 19,
+        "codice": 18,
+        "denominazione": "Organizzazione",
+        "descrizione": "Pianificazione e controllo, Gestione del personale, Procedure e informatizzazione"
+    }, {"id": 20, "codice": 19, "denominazione": "Attivit\u00e0", "descrizione": "Telecomunicazioni, FIO, Varie"}, {
+        "id": 32,
+        "codice": 888888,
+        "denominazione": "88888",
+        "descrizione": "888888"
+    }, {"id": 31, "codice": 999999, "denominazione": "9999", "descrizione": "999999"}]
 };
