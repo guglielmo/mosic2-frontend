89bd9493f8f1a8d2b44f7b01d6e1e1ced9db0a71
Alessandro Curci <hantarex@gmail.com>
2017-04-10 17:56:00 +0200

    added uffici mockdata

---
+++ src/app/_helpers/fake-backend-data/uffici.mockdata.ts
@@ -0,0 +1,351 @@
+export const UfficiMockData: any = {
+    "response": 200,
+    "total_results": 43,
+    "limit": "9999",
+    "offset": 0,
+    "data": [{
+        "id": 1,
+        "codice": 1,
+        "codice_direzione": "",
+        "denominazione": "Ufficio I",
+        "ordine_ufficio": "10",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 2,
+        "codice": 2,
+        "codice_direzione": "",
+        "denominazione": "Ufficio II",
+        "ordine_ufficio": "11",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 3,
+        "codice": 5,
+        "codice_direzione": "",
+        "denominazione": "Ufficio V",
+        "ordine_ufficio": "14",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 4,
+        "codice": 6,
+        "codice_direzione": "",
+        "denominazione": "Ufficio VI",
+        "ordine_ufficio": "15",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 5,
+        "codice": 8,
+        "codice_direzione": "",
+        "denominazione": "Segreteria Direttore",
+        "ordine_ufficio": "18",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 6,
+        "codice": 0,
+        "codice_direzione": "",
+        "denominazione": "",
+        "ordine_ufficio": "",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 7,
+        "codice": 9,
+        "codice_direzione": "",
+        "denominazione": "CUP",
+        "ordine_ufficio": "19",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 8,
+        "codice": 3,
+        "codice_direzione": "",
+        "denominazione": "Ufficio III",
+        "ordine_ufficio": "12",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 9,
+        "codice": 4,
+        "codice_direzione": "",
+        "denominazione": "Ufficio IV",
+        "ordine_ufficio": "13",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 10,
+        "codice": 7,
+        "codice_direzione": "",
+        "denominazione": "Ufficio VII",
+        "ordine_ufficio": "16",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 11,
+        "codice": 10,
+        "codice_direzione": "",
+        "denominazione": "CIPE - Servizio 1",
+        "ordine_ufficio": "20",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 12,
+        "codice": 11,
+        "codice_direzione": "",
+        "denominazione": "CIPE - Servizio 2",
+        "ordine_ufficio": "21",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 13,
+        "codice": 12,
+        "codice_direzione": "",
+        "denominazione": "CIPE - Servizio 3",
+        "ordine_ufficio": "22",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 14,
+        "codice": 13,
+        "codice_direzione": "",
+        "denominazione": "CIPE - Servizio 4",
+        "ordine_ufficio": "23",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 15,
+        "codice": 15,
+        "codice_direzione": "",
+        "denominazione": "R.S.P.U. - Servizio 1",
+        "ordine_ufficio": "24",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 16,
+        "codice": 16,
+        "codice_direzione": "",
+        "denominazione": "R.S.P.U. - Servizio 2",
+        "ordine_ufficio": "25",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 17,
+        "codice": 18,
+        "codice_direzione": "",
+        "denominazione": "DPCPE - Segretariato",
+        "ordine_ufficio": "26",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 18,
+        "codice": 19,
+        "codice_direzione": "1",
+        "denominazione": "Ufficio I - Serv. 1",
+        "ordine_ufficio": "1",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 19,
+        "codice": 20,
+        "codice_direzione": "1",
+        "denominazione": "Ufficio I - Serv. 2",
+        "ordine_ufficio": "2",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 20,
+        "codice": 21,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II - Serv. 1",
+        "ordine_ufficio": "3",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 21,
+        "codice": 22,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II - Serv. 2",
+        "ordine_ufficio": "4",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 22,
+        "codice": 23,
+        "codice_direzione": "3",
+        "denominazione": "Ufficio III - Serv. 1",
+        "ordine_ufficio": "7",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 23,
+        "codice": 24,
+        "codice_direzione": "3",
+        "denominazione": "Ufficio III - Serv. 2",
+        "ordine_ufficio": "8",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 24,
+        "codice": 25,
+        "codice_direzione": "3",
+        "denominazione": "Ufficio III - Serv. 3",
+        "ordine_ufficio": "9",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 25,
+        "codice": 26,
+        "codice_direzione": "",
+        "denominazione": "Ufficio VIII",
+        "ordine_ufficio": "17",
+        "disattivo_ufficio": "1",
+        "solo_delibere": "0"
+    }, {
+        "id": 26,
+        "codice": 27,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II - Serv. 3",
+        "ordine_ufficio": "5",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 27,
+        "codice": 28,
+        "codice_direzione": "",
+        "denominazione": "UTFP",
+        "ordine_ufficio": "10",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 28,
+        "codice": 29,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II - Serv. 4",
+        "ordine_ufficio": "6",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 29,
+        "codice": 30,
+        "codice_direzione": "",
+        "denominazione": "Segreteria Segretario CIPE",
+        "ordine_ufficio": "11",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 30,
+        "codice": 31,
+        "codice_direzione": "",
+        "denominazione": "Ufficio Affari Generali e Personale",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 31,
+        "codice": 32,
+        "codice_direzione": "",
+        "denominazione": "Segreteria Capo Dipartimento",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 32,
+        "codice": 33,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 33,
+        "codice": 34,
+        "codice_direzione": "3",
+        "denominazione": "Ufficio III - Dirigente",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 34,
+        "codice": 35,
+        "codice_direzione": "3",
+        "denominazione": "Ufficio III",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 35,
+        "codice": 36,
+        "codice_direzione": "1",
+        "denominazione": "Ufficio I",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 36,
+        "codice": 37,
+        "codice_direzione": "",
+        "denominazione": "MIP\/CUP",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 37,
+        "codice": 38,
+        "codice_direzione": "",
+        "denominazione": "Segreteria Tecnica programmazione economica",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 38,
+        "codice": 39,
+        "codice_direzione": "2",
+        "denominazione": "NARS",
+        "ordine_ufficio": "12",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "1"
+    }, {
+        "id": 39,
+        "codice": 41,
+        "codice_direzione": "",
+        "denominazione": "Nucleo val. verifica investimenti pubblici",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 40,
+        "codice": 42,
+        "codice_direzione": "",
+        "denominazione": "Dipartimento",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 41,
+        "codice": 43,
+        "codice_direzione": "2",
+        "denominazione": "Ufficio II - Dirigente",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 42,
+        "codice": 44,
+        "codice_direzione": "1",
+        "denominazione": "Ufficio I - Dirigente",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }, {
+        "id": 43,
+        "codice": 45,
+        "codice_direzione": "",
+        "denominazione": "Roma Capitale",
+        "ordine_ufficio": "0",
+        "disattivo_ufficio": "0",
+        "solo_delibere": "0"
+    }]
+};
