8f7142cf0c9a4cfd1a358418b4203ac3ce96da46
Alessandro Curci <hantarex@gmail.com>
2017-05-24 15:16:50 +0200

    FIX missing capabilities check

--- src/app/users/edit/groups-edit.component.ts
+++ src/app/users/edit/groups-edit.component.ts
@@ -44,6 +44,9 @@ export class GroupsEditComponent implements OnInit {
 
     roles: string[] = [];
 
+    public canEdit: boolean = false;
+    public canDelete: boolean = false;
+
     constructor(private router: Router,
                 private route: ActivatedRoute,
                 public apiService: APICommonService
@@ -59,6 +62,8 @@ export class GroupsEditComponent implements OnInit {
 
         this.id = +this.route.snapshot.params['id'];
         this.mode = isNaN(this.id) ? 'create' : 'update';
+        this.canEdit = isNaN(this.id) ? this.apiService.userCan('CREATE_GROUPS') : this.apiService.userCan('EDIT_GROUPS');
+        this.canDelete = this.apiService.userCan('DELETE_GROUPS');
 
         switch( this.mode ) {
             case 'create':
